2c6975b73546b8853d77572b94818388880f7f80
Jason <Jason>
2016-06-04 20:58:17 -0400

    Minion subclass

---
+++ Minion.java
@@ -0,0 +1,18 @@
+public class Minion extends Card {
+
+    private String name, effect;
+    private int health, attack;
+
+    public Minion(String n, int mana, int atk, int hp, String eff) {
+	name = n;
+	manaCost = mana;
+	attack = atk;
+	health = hp;
+	effect = eff;
+    }
+
+    public String toString() {
+	return name;
+    }
+
+}
