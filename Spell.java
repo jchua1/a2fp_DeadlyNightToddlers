262c1184b514fd74b0a7ae8fc007e3f1dca0026f
Jason <Jason>
2016-06-06 00:24:35 -0400

    Removed name and effect (put them in Card.java)

--- Spell.java
+++ Spell.java
@@ -1,12 +1,9 @@
 public class Spell extends Card {
   
-    private String name, effect;
-  
     public Spell(String n, int mana, String eff) {
 	name = n;
 	manaCost = mana;
 	effect = eff;
     }
-
 }
 
