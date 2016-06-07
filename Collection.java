ed84a63a12d3f86afeb0f7c69eacb26922c27e28
Jason <Jason>
2016-06-06 23:52:25 -0400

    Modified organization method and added new filtration method for card classes

--- Collection.java
+++ Collection.java
@@ -15,19 +15,15 @@ public class Collection {
 	try {
 	    FileReader reader = new FileReader("Cards/Minions.txt");
 	    BufferedReader br = new BufferedReader(reader);
-	    String line;
-	    String name;
-	    int mana;
-	    int attack;
-	    int health;
-	    String effect;
+	    String line, name;
+	    int mana, attack, health, clas;
 	    while ((line = br.readLine()) != null) {
 		if (line.equals("{")) {
 		    name = br.readLine();
 		    mana = Integer.parseInt(br.readLine());
 		    attack = Integer.parseInt(br.readLine());
 		    health = Integer.parseInt(br.readLine());
-		    clas = Integer.parseInt(br.readLine());
+		    clas = Integer.parseInt(br.readLine());	      
 		    Card x = new Minion(name,mana,attack,health,clas);
 		    cards.add(x);
 		}
@@ -46,7 +42,7 @@ public class Collection {
 	    String line;
 	    String name;
 	    int mana;
-	    String effect;
+	    int clas;
 	    while ((line = br.readLine()) != null) {
 		if (line.equals("{")) {
 		    name = br.readLine();
@@ -72,7 +68,7 @@ public class Collection {
 	    int mana;
 	    int attack;
 	    int health;
-	    String effect;
+	    int clas;
 	    while ((line = br.readLine()) != null) {
 		if (line.equals("{")) {
 		    name = br.readLine();
@@ -90,7 +86,7 @@ public class Collection {
 	    e.printStackTrace();
 	}
     }
-
+    
     public void organize() {
 	cards = sort(cards);
     }
@@ -101,22 +97,32 @@ public class Collection {
 	int ain = 0;
 	int bin = 0;
 	while (ain < a.size() && bin < b.size()) {
-	    if (a.get(ain).getCost() < b.get(bin).getCost()) {
+	    if (a.get(ain).getClas() < b.get(bin).getClas()) {
 		ret.add(a.get(ain));
 		ain++;
 	    }
-	    else if (a.get(ain).getCost() > b.get(bin).getCost()) {
+	    else if (a.get(ain).getClas() > b.get(bin).getClas()) {
 		ret.add(b.get(bin));
 		bin++;
 	    }
 	    else {
-		if (a.get(ain).toString().compareTo(b.get(bin).toString()) < 0) {
+		if (a.get(ain).getCost() < b.get(bin).getCost()) {
 		    ret.add(a.get(ain));
 		    ain++;
 		}
-		else {
+		else if (a.get(ain).getCost() > b.get(bin).getCost()) {
 		    ret.add(b.get(bin));
 		    bin++;
+		}
+		else {
+		    if (a.get(ain).toString().compareTo(b.get(bin).toString()) < 0) {
+			ret.add(a.get(ain));
+			ain++;
+		    }
+		    else {
+			ret.add(b.get(bin));
+			bin++;
+		    }
 		}
 	    }
 	    i++;
@@ -149,10 +155,10 @@ public class Collection {
 	}
     }
     
-    public ArrayList<Card> filter(int mana) {
+    public ArrayList<Card> filterCost(int mana) {
 	ArrayList<Card> ret = new ArrayList<Card>();
 	for (int i = 0; i < cards.size(); i++) {
-	    if (mana >= 7) {
+	    if (mana == 7) {
 		if (cards.get(i).getCost() >= 7)
 		    ret.add(cards.get(i));
 	    }
@@ -164,15 +170,23 @@ public class Collection {
 	return ret;
     }
 
+    public ArrayList<Card> filterClass(int clas) {
+	ArrayList<Card> ret = new ArrayList<Card>();
+	for (int i = 0; i < cards.size(); i++) {
+	    if (cards.get(i).getClas() == clas)
+		ret.add(cards.get(i));
+	}
+	return ret;
+    }
+
     public static void main(String[] args) {
         Collection x = new Collection();
 	x.addMinions();
 	x.addSpells();
 	x.addWeapons();
-	System.out.println(x.cards);
-	System.out.println(x.cards.size());
 	x.organize();
 	System.out.println(x.cards);
-	System.out.println(x.filter(7));
+	System.out.println(x.filterCost(7));
+	System.out.println(x.filterClass(0));
     }
 }
