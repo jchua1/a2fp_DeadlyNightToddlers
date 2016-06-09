36a9099167625e5184e7360cefeec6aab7cd0296
Jason <jchua@stuy.edu>
2016-06-09 14:12:11 -0400

    Edited constructor

--- Hero.java
+++ Hero.java
@@ -1,13 +1,48 @@
 public class Hero extends Card {
-  public int armor = 0;
-  public String power = "";
+    public int armor, power;
 
-  public Hero() {
-	  health = 30;
+    public Hero(int n) {
+	if (n == 0) {
+	    name = "Druid";
+	    description = "+1 Attack this turn. +1 Armor.";
+	}
+	else if (n == 1) {
+	    name = "Hunter";
+	    description = "Deal 2 damage to the enemy hero.";
+	}
+	else if (n == 2) {
+	    name = "Mage";
+	    description = "Deal 1 damage.";
+	}
+	else if (n == 3) {
+	    name = "Paladin";
+	    description = "Summon a 1/1 Silver Hand Recruit.";
+	}
+	else if (n == 4) {
+	    name = "Priest";
+	    description = "Restore 2 health.";
+	}
+	else if (n == 5) {
+	    name = "Rogue";
+	    description = "Equip a 1/2 Dagger.";
+	}
+	else if (n == 6) {
+	    name = "Shaman";
+	    description = "Summon a random Totem.";
+	}
+	else if (n == 7) {
+	    name = "Warlock";
+	    description = "Draw a card and take 2 damage.";
+	}
+	else if (n == 8) {
+	    name = "Warrior";
+	    description = "Gain 2 armor.";
+	}
+	power = n;
+	health = 30;
+    }	
 
-	}	
-
-  public String toString() {
+    public String toString() {
 	return name;
-  }
+    }
 }
