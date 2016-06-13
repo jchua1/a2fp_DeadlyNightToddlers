7beca5784453599de76939c73d541dd354a2c6d8
Richard <Richard>
2016-06-13 00:31:45 -0400

    small edit

--- Hearthstone.java
+++ Hearthstone.java
@@ -131,7 +131,6 @@ public class Hearthstone {
 	    	Engine.draw(Engine.opponentHand,Engine.opponentDeck,1);
 
 	    while (!choice.toUpperCase().equals("END")) {
-		Engine.clearConsole();
 		Graphics.showBoard();
 		System.out.println ("What will you do next?");
 		choice = in.nextLine();
